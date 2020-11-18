4f4b2b3c4340a57c41c01532b3eece5e45a76bc8
zefengqiu1 <zqiu6@go.pasadena.edu>
2020-11-18 14:09:04 -0800

    inventory

--- frontend/src/pages/admin/products/Edit.js
+++ frontend/src/pages/admin/products/Edit.js
@@ -98,6 +98,17 @@ function Edit(props) {
             initialValue: currentData.price
           })(<Input placeholder="Please fill in product price" />)}
         </Form.Item>
+        <Form.Item label="Inventory">
+          {getFieldDecorator("inventory", {
+            rules: [
+              {
+                required: true,
+                message: "Please fill in inventory"
+              }
+            ],
+            initialValue: currentData.inventory
+          })(<Input placeholder="Please fill in inventory" />)}
+        </Form.Item>
         <Form.Item name={['user', 'introduction']} label="Description">
         {getFieldDecorator("description", {
             initialValue: currentData.description
