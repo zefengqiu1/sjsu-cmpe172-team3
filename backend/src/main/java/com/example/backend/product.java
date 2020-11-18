37ae8e39cb431496db79c4ab5cc02cd69454d71d
zefengqiu1 <zqiu6@go.pasadena.edu>
2020-11-17 17:40:33 -0800

    folder structure

--- backend/src/main/java/com/example/backend/product.java
+++ backend/src/main/java/com/example/backend/product.java
@@ -13,6 +13,16 @@ public class product {
     private int price;
     private String description;
 
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    private int stock;
+
     public long getId() {
         return id;
     }
