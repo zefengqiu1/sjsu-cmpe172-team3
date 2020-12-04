1164c210201b26a5e02e1c14e168abf19864f5d8
Thiefre <kevinvluu@gmail.com>
2020-12-04 02:06:56 -0800

    Removed credentials

--- backend/src/main/java/com/example/backend/BackendApplication.java
+++ backend/src/main/java/com/example/backend/BackendApplication.java
@@ -1,18 +1,15 @@
 package com.example.backend;
 
-import com.alibaba.fastjson.JSONObject;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-import java.util.List;
-import java.util.Map;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @CrossOrigin("*")
+@PropertySource("./dev.properties")
 public class BackendApplication {
-
-
+	
     public static void main(String[] args) {
         SpringApplication.run(BackendApplication.class, args);
     }
